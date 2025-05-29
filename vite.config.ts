80a4bfdeeb9b9e0755232635b64a6994bae6d473
Vincent Taylor <vincent.sam.taylor@gmail.com>
2025-05-29 15:34:15 +0200

    fix tailwind/vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -7,6 +7,5 @@ export default defineConfig({
     watch: {
       ignored: ['**/server/db.json'],
     },
-    port: 3000,
   },
-})
+});
