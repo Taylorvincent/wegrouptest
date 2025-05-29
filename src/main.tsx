1d9d49a6a2a7c682f5bab17d02cff0a936a5afe8
Vincent Taylor <vincent.sam.taylor@gmail.com>
2025-05-29 14:54:12 +0200

    1. tailwind stuff 2. strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 // Enable React Strict Mode if you want to use it (components will be rendered twice)
-createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  // </StrictMode>,
-)
+  </React.StrictMode>
+);
